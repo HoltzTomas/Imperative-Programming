67af294a7a27a121fd21c24c54eddd4182af3383
Tomas Holtz <tomiholtz@gmail.com>
2024-09-10 18:04:31 -0300

    Ej 11 passed tests

--- TP N6/ej_11.c
+++ TP N6/ej_11.c
@@ -5,51 +5,103 @@
 
 // Para ordenar se puede usar el bubble sort ( https://www.geeksforgeeks.org/bubble-sort/ ) o selection sort (https://www.geeksforgeeks.org/selection-sort/ )
 
+#include <assert.h>
+#include <stdio.h>
+
 #define MAXCOL 10
 
-void sortByColumn(unsigned char matrix[][MAXCOL], int dimRows, int dimCols, int colOrd);
-void swapRows(unsigned char matrix[][MAXCOL], int dimCols, int row1, int row2);
+void sortByColumn(int matrix[][MAXCOL], int dimRows, int dimCols, int colOrd);
+void swapRows(int matrix[][MAXCOL], int dimCols, int row1, int row2);
 
-void swapColumns(unsigned char matrix[][MAXCOL], int dimCols, int row1, int row2){
-            // Quiero swapear las columnas
-            unsigned char temp[dimCols];
-
-            //Me guardo el vector de la fila 1
-            for (int j = 0; j < dimCols; j++)
-            {
-                temp[j] = matrix[row1][j];
-            }
-
-            //Me guardo la fila 2 en fila 1
-            for (int j = 0; j < dimCols; j++)
-            {
-                matrix[row1][j] = matrix[row2][j];
-            }
-
-            //Guardo aux en fila 2
-            for (int j = 0; j < dimCols; j++)
-            {
-                matrix[row2][j] = temp[j];
-            }
+int checkVector(const int v1[], const int v2[], int dim)
+{
+    for (int i = 0; i < dim; i++)
+        if (v1[i] != v2[i])
+            return 0;
+    return 1;
 }
 
-// Selection sort way
-void sortByColumn(unsigned char matrix[][MAXCOL], int dimRows, int dimCols, int colOrd)
+int main(void)
+{
+    int m[][MAXCOL] = {{1, 2, 3, 4, 5},
+                       {6, 2, 8, 9, 10},
+                       {3, 5, 8, 2, 1},
+                       {8, 7, 3, 6, 7}};
+
+    int m2[][MAXCOL] = {{1, 2, 3, 4, 5},
+                        {6, 2, 8, 9, 10},
+                        {3, 5, 8, 2, 1},
+                        {8, 7, 3, 6, 7}};
+
+    // La matriz no cambia
+    sortByColumn(m, 4, 5, 2);
+
+    for (int i = 0; i < 4; i++)
+        assert(checkVector(m[i], m2[i], 5));
+
+    sortByColumn(m, 4, 5, 1);
+    assert(checkVector(m[0], m2[0], 5));
+    assert(checkVector(m[1], m2[2], 5));
+    assert(checkVector(m[2], m2[1], 5));
+    assert(checkVector(m[3], m2[3], 5));
+
+    sortByColumn(m, 4, 5, 3);
+    assert(checkVector(m[0], m2[0], 5) || checkVector(m[0], m2[3], 5));
+    assert(checkVector(m[1], m2[0], 5) || checkVector(m[1], m2[3], 5));
+    assert(checkVector(m[2], m2[1], 5) || checkVector(m[2], m2[2], 5));
+    assert(checkVector(m[3], m2[1], 5) || checkVector(m[3], m2[2], 5));
+
+    printf("OK!\n");
+    return 0;
+}
+
+void swapRows(int matrix[][MAXCOL], int dimCols, int row1, int row2)
+{
+    // Quiero swapear las filas
+    int temp[MAXCOL];  // Usa int para las filas en lugar de unsigned char.
+
+    // Me guardo el vector de la fila 1
+    for (int j = 0; j < dimCols; j++)
+    {
+        temp[j] = matrix[row1][j];
+    }
+
+    // Me guardo la fila 2 en fila 1
+    for (int j = 0; j < dimCols; j++)
+    {
+        matrix[row1][j] = matrix[row2][j];
+    }
+
+    // Guardo temp en fila 2
+    for (int j = 0; j < dimCols; j++)
+    {
+        matrix[row2][j] = temp[j];
+    }
+}
+
+// Selection sort para ordenar las filas basadas en una columna específica
+void sortByColumn(int matrix[][MAXCOL], int dimRows, int dimCols, int colOrd)
 {
     int min_index;
-    // The first step is to go through the entire vector and find the smallest value
-    // We gonna do that dimCols - 1 times
-    for (int i = 0; i < dimCols - 1; i++)
+    colOrd--;  // Ajustar colOrd para que sea 0-indexado.
+    
+    for (int i = 0; i < dimRows - 1; i++)
     {
         min_index = i;
-        // Find the min
-        for (int j = 1; j < dimCols; j++)
-            if (matrix[j][colOrd] < matrix[min_index][colOrd])
-                min_index = j;
-
-        // Swap if i wasnt the min
-        if (min_index != i)
-            swapColumns(matrix, dimCols, i, min_index);
         
+        // Encontrar el índice de la fila con el valor mínimo en la columna colOrd
+        for (int j = i + 1; j < dimRows; j++)
+        {
+            if (matrix[j][colOrd] < matrix[min_index][colOrd])
+            {
+                min_index = j;
+            }
+        }
+
+        // Swap si la fila mínima no es la actual
+        if (min_index != i)
+        {
+            swapRows(matrix, dimCols, i, min_index);
+        }
     }
 }
